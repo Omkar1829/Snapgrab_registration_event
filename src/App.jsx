6d66d7852d813e1ad993da60801747eee8768134
Ashish Shinde <ashishshindeoffice72@gmail.com>
2025-12-09 18:10:23 +0530

    Fix merge conflicts and clean up routing in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,24 @@
 import React from 'react'
-<<<<<<< HEAD
 import Userdashboard from './components/Userdashboard'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Camera from './components/camera'
-=======
 import { BrowserRouter, Routes, Route } from "react-router-dom";
->>>>>>> fffbcc7b5e7c2a9839b25f646066f133c1ca7c0c
 
 import AdminDashboard from './Pages/AdminDashBord.jsx';
 const App = () => {
   return (
-<<<<<<< HEAD
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Userdashboard />} />
-          <Route path='cam' element={<Camera/>} />
+          <Route path='cam' element={<Camera />} />
+          <Route path="/admin" element={<AdminDashboard />} />
+
+
         </Routes>
       </BrowserRouter>
-    </>
-=======
-    <BrowserRouter>
-      <Routes>
 
-        
-        <Route path="/admin" element={<AdminDashboard/>} />
-       
-
-      </Routes>
-    </BrowserRouter>
->>>>>>> fffbcc7b5e7c2a9839b25f646066f133c1ca7c0c
-  )
+    </>)
 }
 
 export default App
